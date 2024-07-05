3c1c31ff3d369dfe8fc0e390c981e9827b90b74f
MaheepChaudhary <my email address used on my git repo>
2024-07-05 13:33:04 +0530

    [Eval GPT-2]: SAE + Masking training for GPT-2

--- gpt2/models.py
+++ gpt2/models.py
@@ -182,7 +182,7 @@ class my_model(nn.Module):
     def forward(self, source_ids, base_ids, temperature):
         
         # l4_mask_sigmoid = t.sigmoid(self.l4_mask / temperature)
-        l4_mask_sigmoid = self.l4_mask
+        # l4_mask_sigmoid = self.l4_mask
         
         if self.method == "neuron masking":
             with self.model.trace() as tracer:
