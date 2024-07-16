b1efff8bd47f267e45d8a232c05d23ba39c469f2
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-07-16 07:24:44 -0700

    llama ele sae init

--- llama3b/exp.py
+++ llama3b/exp.py
@@ -1,26 +1,35 @@
 import os
 from sae.sae import Sae
-
-sae = Sae.load_from_hub("EleutherAI/sae-llama-3-8b-32x", hookpoint="layers.10")
+from nnsight import LanguageModel 
+import torch as t
 from huggingface_hub import login
 from transformers import AutoModel, AutoTokenizer
 
-# Retrieve the token from Colab secrets
-hf_token = os.getenv('HF_TOKEN')
+n_llama_model = LanguageModel("meta-llama/Meta-Llama-3-8B", device_map = t.device("cuda:1"))
+sae = Sae.load_from_hub("EleutherAI/sae-llama-3-8b-32x", hookpoint="layers.1").to(t.device("cuda:1"))
+print(n_llama_model)
 
-# Log in with your token
-login(token=hf_token)
+with n_llama_model.trace("my_name_is_maheep") as tracer:
+    outputl1 = n_llama_model.model.layers[0].output[0].save()
+    
+top_acts, top_indices  = sae.select_topk(outputl1)
+d1 = sae.decode(top_acts, top_indices)
+
+print(d1.shape)
+#print(top_indices)
+print(outputl1.shape)
+
+
 
 model_name = "meta-llama/Meta-Llama-3-8B"
 tokenizer = AutoTokenizer.from_pretrained(model_name, token="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")
 model = AutoModel.from_pretrained(model_name, token="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")
 
+inputs = tokenizer("Hello, world!", return_tensors="pt")
 
-# inputs = tokenizer("Hello, world!", return_tensors="pt")
+with t.inference_mode():
+    outputs = model(**inputs, output_hidden_states=True)
 
-# with t.inference_mode():
-#     outputs = model(**inputs, output_hidden_states=True)
-
-#     latent_acts = []
+    latent_acts = []
 #     for sae, hidden_state in zip(saes.values(), outputs.hidden_states):
-#         latent_acts.append(sae.encode(hidden_state))
+#1         latent_acts.append(sae.encode(hidden_state))
