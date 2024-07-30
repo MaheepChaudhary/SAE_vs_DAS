c89e781457bbf751bd1296d71ef1883610549ef9
maheepchaudhary <chaudhary.maheep28@gmail.com>
2024-07-30 06:54:29 -0700

    debugging

--- gpt2/models.py
+++ gpt2/models.py
@@ -221,7 +221,7 @@ class my_model(nn.Module):
             # neuron_dim = (1,self.token_length_allowed, 768)
             neuron_dim = (1, 768)
             self.l4_mask = t.nn.Parameter(
-                t.zeros(neuron_dim, device=DEVICE), requires_grad=False
+                t.zeros(neuron_dim, device=DEVICE), requires_grad=True
             )
             self.l4_mask = self.l4_mask.to(DEVICE)
 
