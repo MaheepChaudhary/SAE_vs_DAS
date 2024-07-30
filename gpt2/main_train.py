dad2e1eb156155a145394f25d33f473b15079f4d
maheepchaudhary <chaudhary.maheep28@gmail.com>
2024-07-30 08:19:03 -0700

    u

--- gpt2/main_train.py
+++ gpt2/main_train.py
@@ -585,7 +585,7 @@ if __name__ == "__main__":
         "-lr",
         "--learning_rate",
         default=0.001,
-        type=int,
+        type=float,
         help="learning rate for the optimizer",
     )
     parser.add_argument(
@@ -668,7 +668,7 @@ if __name__ == "__main__":
     # TODO: The total number of batches is total_no_samples/batch_len
     batch_size = args.batch_size
     target_total_step = len(train_data) * args.epochs
-    temperature_start = 20.0
+    temperature_start = 10.0
     temperature_end = 0.1
     temperature_schedule = (
         t.linspace(
