8fcbd4308ab2fe7c06fa270653fa066fe7de8c4d
maheepchaudhary <chaudhary.maheep28@gmail.com>
2024-07-31 05:07:01 -0700

    u

--- gpt2/gpt_run.sh
+++ gpt2/gpt_run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-
 echo "Running neuron masking script"
 python main_train.py -a country -tla 61 -method "neuron masking" -e 10 -t train -idd "country" -bs 128 -wb "True" -n "t(10-0.1)_lr0.01_layer-0" -lr "0.01" -lid 0
 python main_train.py -a continent -tla 61 -method "neuron masking" -e 10 -t train -idd "continent" -bs 128 -wb "True" -n "t(10-0.1)_lr0.01_layer-0" -lr "0.01" -lid 0
