c580949a200f5a2c580b1d0140860084d1f34d10
maheepchaudhary <chaudhary.maheep28@gmail.com>
2024-08-01 00:37:07 -0700

    u

--- gpt2/gpt_run.sh
+++ gpt2/gpt_run.sh
@@ -7,8 +7,8 @@
 #
 #
 echo "Running das masking script"
-python main_train.py -a country -tla 61 -method "das masking" -e 10 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.01_layer-0" -lr "0.01" -lid 0
-python main_train.py -a continent -tla 61 -method "das masking" -e 10 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.01_layer-0" -lr "0.01" -lid 0
+python main_train.py -a country -tla 61 -method "das masking" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-0" -lr "0.001" -lid 0
+python main_train.py -a continent -tla 61 -method "das masking" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-0" -lr "0.001" -lid 0
 
 
 # echo "Running sae masking neel's script"
