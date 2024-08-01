a294781b4ee30a064157b47655c63c26b4a90f01
MaheepChaudhary <my email address used on my git repo>
2024-08-01 17:46:45 +0530

    run modification

--- gpt2/gpt_run.sh
+++ gpt2/gpt_run.sh
@@ -2,8 +2,8 @@
 
 
 echo "running neuron masking script"
-python main_train.py -a country -tla 61 -method "neuron masking" -e 20 -t train -idd "country" -bs 16 -wb "true" -n "t(10-0.1)_lr0.001_layer-0" -lr "0.001" -lid 0
-python main_train.py -a continent -tla 61 -method "neuron masking" -e 20 -t train -idd "continent" -bs 16 -wb "true" -n "t(10-0.1)_lr0.001_layer-0" -lr "0.001" -lid 0
+python main_train.py -a country -tla 61 -method "neuron masking" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-0" -lr "0.001" -lid 0
+python main_train.py -a continent -tla 61 -method "neuron masking" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-0" -lr "0.001" -lid 0
 
 
 # echo "Running das masking script"
