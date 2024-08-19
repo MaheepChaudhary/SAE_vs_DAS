40d13d6d5a5557d791aa81f4d496e002323cd14f
MaheepChaudhary <my email address used on my git repo>
2024-08-19 21:56:06 +0530

    layer 4 runnning

--- gpt2/gpt_run.sh
+++ gpt2/gpt_run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 
-#echo "running neuron masking script"
-#python3.11 main_train.py -a country -tla 61 -method "neuron masking" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2 
-#python3.11 main_train.py -a continent -tla 61 -method "neuron masking" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2 
+echo "running neuron masking script"
+python3.11 main_train.py -a country -tla 61 -method "neuron masking" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4 
+python3.11 main_train.py -a continent -tla 61 -method "neuron masking" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4 
+
+
+echo "Running das masking script"
+python3.11 main_train.py -a country -tla 61 -method "das masking" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4
+python3.11 main_train.py -a continent -tla 61 -method "das masking" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4
+
+echo "Running sae masking neel's script"
+python3.11 main_train.py -a country -tla 61 -method "sae masking neel" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4
+python3.11 main_train.py -a continent -tla 61 -method "sae masking neel" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4
+
+ echo "Running sae masking openai script"
+ python3.11 main_train.py -a country -tla 61 -method "sae masking openai" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-4" -lr "0.001" -lid 4
+ python3.11 main_train.py -a continent -tla 61 -method "sae masking openai" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr00.01_layer-4" -lr "0.001" -lid 4
+
+
+# echo "Running sae masking apollo script"
+# python3.11 main_train.py -a country -tla 61 -method "sae masking apollo" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-1" -lr "0.001" -lid 1
+# python3.11 main_train.py -a continent -tla 61 -method "sae masking apollo" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr00.01_layer-1" -lr "0.001" -lid 1
 #
-#
-#echo "Running das masking script"
-#python3.11 main_train.py -a country -tla 61 -method "das masking" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2
-#python3.11 main_train.py -a continent -tla 61 -method "das masking" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2
-#
-#echo "Running sae masking neel's script"
-#python3.11 main_train.py -a country -tla 61 -method "sae masking neel" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2
-#python3.11 main_train.py -a continent -tla 61 -method "sae masking neel" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2
-
-# echo "Running sae masking openai script"
-# python3.11 main_train.py -a country -tla 61 -method "sae masking openai" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-2" -lr "0.001" -lid 2
-# python3.11 main_train.py -a continent -tla 61 -method "sae masking openai" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr00.01_layer-2" -lr "0.001" -lid 2
-
-
-echo "Running sae masking apollo script"
-python3.11 main_train.py -a country -tla 61 -method "sae masking apollo" -e 20 -t train -idd "country" -bs 16 -wb "True" -n "t(10-0.1)_lr0.001_layer-1" -lr "0.001" -lid 1
-python3.11 main_train.py -a continent -tla 61 -method "sae masking apollo" -e 20 -t train -idd "continent" -bs 16 -wb "True" -n "t(10-0.1)_lr00.01_layer-1" -lr "0.001" -lid 1
-
