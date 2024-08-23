4dcd3358019bac1d794e012be9016efce81c350c
MaheepChaudhary <my email address used on my git repo>
2024-08-23 10:10:06 +0530

    apollo sae eval

--- gpt2/eval_run.sh
+++ gpt2/eval_run.sh
@@ -3,3 +3,7 @@ python3.11 eval_sae_main.py -d "cuda:1" -met "sae masking neel" -bs 16
 
 echo "Running eval for OpenAI SAE"
 python3.11 eval_sae_main.py -d "cuda:1" -met "sae masking openai" -bs 16  
+
+
+echo "Running eval for Apollo SAE"
+python3.11 eval_sae_main.py -d "cuda:1" -met "sae masking apollo" -bs 16  
