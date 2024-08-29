8146add151b4bb60be71a5479e57e99b9a81c69a
MaheepChaudhary <my email address used on my git repo>
2024-08-29 18:32:07 +0530

    latex graph for reconstruction

--- gpt2/exp.py
+++ gpt2/exp.py
@@ -1,6 +1,45 @@
-def read_file_in_batches(filename, batch_size=11):
-    latex_final_list = []
+def create_latex_table(data, headers):
+    # Begin the LaTeX table environment
+    latex_code = "\\begin{table}[h!]\n"
+    latex_code += "\\centering\n"
+    latex_code += "\\begin{tabular}{|" + " | ".join(["c"] * len(headers)) + "|}\n"
+    latex_code += "\\hline\n"
 
+    # Add headers
+    latex_code += " & ".join(headers) + " \\\\\n"
+    latex_code += "\\hline\n"
+
+    # Add table data
+    for row in data:
+        latex_code += " & ".join(map(str, row)) + " \\\\\n"
+        latex_code += "\\hline\n"
+
+    # End the LaTeX table environment
+    latex_code += "\\end{tabular}\n"
+    latex_code += "\\caption{Your caption here}\n"
+    latex_code += "\\label{table:your_label}\n"
+    latex_code += "\\end{table}"
+
+    return latex_code
+
+
+def read_file_in_batches(filename, batch_size=11):
+    latex_final_list = [
+        [
+            "Layer 0",
+            "Layer 1",
+            "Layer 2",
+            "Layer 3",
+            "Layer 4",
+            "Layer 5",
+            "Layer 6",
+            "Layer 7",
+            "Layer 8",
+            "Layer 9",
+            "Layer 10",
+            "Layer 11",
+        ]
+    ]
     # Read the entire file and split into lines
     with open(filename, "r") as file:
         lines = [line.strip() for line in file.readlines()]
@@ -16,7 +55,14 @@ def read_file_in_batches(filename, batch_size=11):
 # Example usage
 filename = "latex_table.txt"
 latex_final_list = read_file_in_batches(filename)
+headers = [
+    "Bloom SAE Continent",
+    "Bloom SAE Country",
+    "OpenAI SAE Continent",
+    "OpenAI SAE Country",
+    "Apollo SAE Continent",
+    "Apollo SAE Country",
+]
+create_latex_table(latex_final_list, headers)
 
 # Output the batches
-for idx, batch in enumerate(latex_final_list):
-    print(f"Batch {idx + 1}: {batch}")
